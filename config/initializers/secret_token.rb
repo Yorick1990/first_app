0d245f03fb716a6ba881587a83cef5316c9ed682
Yorick <yorick.dieng@gmail.com>
2015-02-10 15:20:45 +0100

    Revert "test"
    This reverts commit e0c75a7f632ad51c3e00cd33f692058aac9bd894.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '80cf16edad912ef7548c84a8eb98dd910dc97f89666586a151bb8b63f11bcd17589dc77b1cba6efa740516d692bad8414da0e265462becf09473483185e5728b'
